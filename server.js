4b2b44f23c463a5f4e3b3e6c54c985c0e1cf9966
Bipin Rai <bipi0007@edu.easj.dk>
2020-03-25 01:24:14 +0100

    app name

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Install express server
-const express = require('express');
-const path = require('path');
- 
+const express = require("express");
+const path = require("path");
+
 const app = express();
- 
+
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/bipindaju'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/bipindaju/index.html'));
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
- 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
