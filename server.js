5f8a0d4fe4f1b262a9f06caec6fc67489b6c26f4
Bipin Rai <bipi0007@edu.easj.dk>
2020-03-25 01:38:29 +0100

    app name

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + "/everest-services"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "index.html"));
+  res.sendFile(path.join(__dirname + "everest-services/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
