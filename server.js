9dbbca26c3d97ef04486cfbe537bd96f0cb7cd7f
Bipin Rai <bipi0007@edu.easj.dk>
2020-03-25 13:11:28 +0100

    try

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + "/everest-services"));
+app.use(express.static(everest-services));
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "everest-services/index.html"));
 });
+console.log("Logging directory path");
+console.log("Logging directory path" + __dirname);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
