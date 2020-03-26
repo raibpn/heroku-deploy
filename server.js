b7f202268eadfc3a16524adc386bdfebfb6ac9a1
Bipin Rai <bipi0007@edu.easj.dk>
2020-03-26 23:05:18 +0100

    try

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('.dist/everest-services'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + './dist/everest-services/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/everest-services/index.html'));
 });
 console.log("Logging directory path");
 console.log("Logging directory path" + __dirname);
