7a87f620dddb01c016a7a32ce7e3d9cb8deb293e
Bipin Rai <bipi0007@edu.easj.dk>
2020-03-26 23:40:13 +0100

    try

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static('./dist/heroku-deploy'));
+app.use(express.static("./dist/heroku-deploy"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname,'./dist/heroku-deploy'));
+  res.sendFile(path.join(__dirname, "./dist/heroku-deploy/index.html"));
 });
 console.log("Logging directory path");
 console.log("Logging directory path" + __dirname);
