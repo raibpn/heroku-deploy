2107621c09e729e784a3973cd51cd731d3a0d694
Bipin Rai <bipi0007@edu.easj.dk>
2020-03-27 01:20:09 +0100

    index.js changed launch.json

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the angularapp directory
-//app.use(express.static("./dist/heroku-deploy"));
+app.use(express.static("./dist/Everest-Services"));
 
 app.get("*", function(req, res) {
   const index = path.join(__dirname, "build", "index.html");
   res.sendFile(index);
 });
-console.log("Logging directory path");
-console.log("Logging directory path" + __dirname);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
